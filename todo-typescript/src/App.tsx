938694f299bc8cd5cb7cd723da80bb0febf66d29
Alexander <voprosy262@gmail.com>
2023-11-16 22:52:32 +0300

    feat: add types for redux

--- todo-typescript/src/App.tsx
+++ todo-typescript/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import { useAppDispatch } from './hooks';
+import { useState, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from './hooks';
 
-import { addTodo } from './store/todoSlice';
+import { fetchTodos, addNewTodo } from './store/todoSlice';
 import NewTodoForm from './components/NewTodoForm';
 import TodoList from './components/TodoList';
 
@@ -9,14 +9,19 @@ import './App.css';
 
 const App: React.FC = () => {
   const [text, setText] = useState('');
+  const { loading, error } = useAppSelector((state) => state.todos);
   const dispatch = useAppDispatch();
 
   const handleAction = () => {
     if (text.trim().length) {
-      dispatch(addTodo(text));
+      dispatch(addNewTodo(text));
       setText('');
     }
   };
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
 
   return (
     <div className="App">
@@ -25,6 +30,8 @@ const App: React.FC = () => {
         updateText={setText}
         handleAction={handleAction}
       />
+      {loading && <h2>Loading...</h2>}
+      {error && <h2>An error occured: {error}</h2>}
       <TodoList />
     </div>
   );
